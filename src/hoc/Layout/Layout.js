af0d4f27820046328a41991cbfc353a7f191b183
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-05-31 12:54:17 +0200

    Add prop-types and modify Layout.js

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Layout extends Component {
   render() {
+    const { children } = this.props;
     return (
       <>
         <p>Navigation Bar</p>
-        <main>Main</main>
+        <main>{children}</main>
       </>
     );
   }
 }
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export default Layout;
