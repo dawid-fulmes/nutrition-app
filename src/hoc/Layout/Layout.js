4eb4415a6de46415ea1c9d991cd325c9905f2175
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-05-31 14:22:25 +0200

    Add ThemeProvider and theme to LAyout.js

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { ThemeProvider } from "styled-components";
+
 import GlobalStyle from "../../assets/styles/GlobalStyle/GlobalStyle";
+import { theme } from "../../assets/styles/theme/theme";
 
 class Layout extends Component {
   render() {
     const { children } = this.props;
     return (
-      <>
-        <GlobalStyle />
-        <p>Navigation Bar</p>
-        <main>{children}</main>
-      </>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <p>Navigation Bar</p>
+          <main>{children}</main>
+        </>
+      </ThemeProvider>
     );
   }
 }
