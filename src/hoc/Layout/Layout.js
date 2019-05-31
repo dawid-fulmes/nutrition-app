1faa58ac069660638de2940a1d835561882aeaae
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-05-31 13:27:58 +0200

    Create GlobalStyle

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import GlobalStyle from "../../assets/styles/GlobalStyle/GlobalStyle";
 
 class Layout extends Component {
   render() {
     const { children } = this.props;
     return (
       <>
+        <GlobalStyle />
         <p>Navigation Bar</p>
         <main>{children}</main>
       </>
