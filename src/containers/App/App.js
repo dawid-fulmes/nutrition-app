50b41891997f63986f4211d93fe8c92d1e5af92d
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-06-04 20:19:44 +0200

    Add enzyme, configure it and make simple test for App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import Layout from "../../hoc/Layout/Layout";
 
 function App() {
-  return <Layout>Main</Layout>;
+  return (
+    <Layout>
+      <div>main</div>
+    </Layout>
+  );
 }
 
 export default App;
