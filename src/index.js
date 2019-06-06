853b04b086a7479b6fbeca2a5a25338e792815f2
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-06-06 13:40:51 +0200

    Add initial Redux store
    Install axios, redux, react-redux, redux-thunk. Set initial state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./containers/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import App from "./containers/App/App";
+import reducer from "./store/reducers/search";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
