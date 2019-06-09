e2f1711b5867da1f735a68b2cd3977388e228788
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-06-09 11:18:44 +0200

    Fix About page

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -11,10 +11,16 @@ const About = () => {
   return (
     <StyledWrapper>
       <h1>About Project</h1>
-      <p>It is demo version of the nutrition app.</p>
+      <p>It is demo version of the Nutrition App.</p>
       <p>
         Check a repo{" "}
-        <a href="https://github.com/dawid-fulmes/nutrition-app">here</a>
+        <a
+          href="https://github.com/dawid-fulmes/nutrition-app"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          here
+        </a>
       </p>
     </StyledWrapper>
   );
