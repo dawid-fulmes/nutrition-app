af2035d18cd0dd265ddf0111ffe46af73410971f
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-06-09 11:55:24 +0200

    Updates

---
+++ precache-manifest.88ac45303987281a355543b1b7d87f1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e5d4cde2196b7071b43f949b8b6f4d",
+    "url": "/nutrition-app/index.html"
+  },
+  {
+    "revision": "247e851edaaa8e318bae",
+    "url": "/nutrition-app/static/js/2.7227a2d8.chunk.js"
+  },
+  {
+    "revision": "b9911a5d5f03d9eece83",
+    "url": "/nutrition-app/static/js/main.bcea492c.chunk.js"
+  },
+  {
+    "revision": "9ea9a0025739f3f8c838",
+    "url": "/nutrition-app/static/js/runtime~main.aff253eb.js"
+  }
+]);
