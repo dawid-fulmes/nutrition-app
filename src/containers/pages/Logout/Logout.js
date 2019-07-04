572ed54b53c7807c9379e6f3cc9e2b387fd34f80
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-07-04 08:37:28 +0200

    Add agreement in logout page

--- src/containers/pages/Logout/Logout.js
+++ src/containers/pages/Logout/Logout.js
@@ -1,26 +1,50 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
 import { authLogout } from "../../../store/actions/auth";
+import Button from "../../../components/Button/Button";
 
-const Logout = ({ onLogout }) => {
-  useEffect(() => {
-    onLogout();
-  });
+const StyledWrapper = styled.div`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 1rem;
+  text-align: center;
+  margin: auto;
+  padding: 20px;
+  & > button {
+    margin: 10px;
+  }
+`;
 
-  return <Redirect to="/" />;
+const Logout = ({ isAuth, onLogout, history }) => {
+  return isAuth ? (
+    <StyledWrapper>
+      <h1>Are You sure?</h1>
+      <Button click={onLogout}>Yes</Button>
+      <Button click={history.goBack}>No</Button>
+    </StyledWrapper>
+  ) : (
+    <Redirect to="/" />
+  );
 };
 
 Logout.propTypes = {
   onLogout: PropTypes.func.isRequired,
+  isAuth: PropTypes.bool.isRequired,
+  history: PropTypes.object.isRequired,
 };
+
+const mapStateToProps = state => ({
+  isAuth: !!state.auth.token,
+});
 
 const mapDispatchToProps = dispatch => ({
   onLogout: () => dispatch(authLogout()),
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Logout);
